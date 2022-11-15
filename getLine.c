cd72838f627f9bf2567b29eccdc09d98c39be759
identi-fire <52828895+identi-fire@users.noreply.github.com>
2022-11-15 22:47:25 +0100

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
+ * input_buf - buffers chained commands.
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
