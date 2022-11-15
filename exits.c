6330608b8257cc2fed93112dcd24198ad439445e
identi-fire <52828895+identi-fire@users.noreply.github.com>
2022-11-15 22:38:22 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
+ **_strncpy - copies a string.
  *@dest: the destination string to be copied to
  *@src: the source string
  *@n: the amount of characters to be copied
