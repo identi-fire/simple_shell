64434f2e923896616da84bc88bbd38596e85595b
identi-fire <52828895+identi-fire@users.noreply.github.com>
2022-11-15 22:32:43 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -4,7 +4,7 @@
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *  Return: exits with a given exit status
+ *  Return: exits with a given exit status.
  *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
