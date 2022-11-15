0795fe891cfd150a6952dc570d0a16a0d8d8f956
identi-fire <52828895+identi-fire@users.noreply.github.com>
2022-11-15 22:33:22 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -81,7 +81,7 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
+ * _myalias - mimics the alias builtin (man alias).
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
