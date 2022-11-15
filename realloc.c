d8cc897c58b17678488e566f5f1593885e0e937b
identi-fire <52828895+identi-fire@users.noreply.github.com>
2022-11-15 22:46:09 +0100

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -5,7 +5,7 @@
  *@s: the pointer to the memory area
  *@b: the byte to fill *s with
  *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
+ *Return: (s) a pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
